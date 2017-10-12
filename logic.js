0b9ca3cca4d2300efaec09b566d2894f27ed20cc
Bobby Angus <Bobby Angus>
2017-10-11 20:27:26 -0400

    Working countdown

--- logic.js
+++ logic.js
@@ -15,7 +15,7 @@ var config = {
 var database = firebase.database();
 
 // // // --------- Authentication code ------ // // //
-// Track the UID of the current user.  
+Track the UID of the current user.  
      var currentUid = null;  
      firebase.auth().onAuthStateChanged(function(user) {  
       // onAuthStateChanged listener triggers every time the user ID token changes.  
@@ -159,3 +159,35 @@ $("#submit-bid").on("click", function(event) {
   }
 
 });
+
+
+//Countdown Function
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2018 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
