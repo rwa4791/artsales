17f1e4fcb7773992477e6e26623e38ba43cd20a3
Bobby Angus <Bobby Angus>
2017-10-13 21:03:53 -0400

    changes

--- logic.js
+++ logic.js
@@ -18,7 +18,7 @@ var database = firebase.database();
 
 // -----------------------------
 // // // --------- Authentication code ------ // // //
-//Track the UID of the current user.  
+Track the UID of the current user.  
      var currentUid = null;  
      firebase.auth().onAuthStateChanged(function(user) {  
       // onAuthStateChanged listener triggers every time the user ID token changes.  
