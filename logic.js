bb5cb3301598aef9112629345a18868293ef4b43
Bobby Angus <Bobby Angus>
2017-10-14 09:53:40 -0400

    update timer

--- logic.js
+++ logic.js
@@ -191,7 +191,7 @@ database.ref("/bidderData").limitToLast(1).on("child_added", function(snapshot) 
 
 //Countdown Function
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 13, 2017 24:00:00").getTime();
+var countDownDate = new Date("Oct 13, 2018 24:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
